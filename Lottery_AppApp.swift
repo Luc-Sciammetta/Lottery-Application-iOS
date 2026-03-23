737fd2a4436cedfbfdfc132f4cf4a12b8118bb79
Luc-Sciammetta <luc.sciammetta@gmail.com>
2026-03-23 19:51:32 -0400

    initial commit

---
+++ Lottery_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lottery_AppApp.swift
+//  Lottery App
+//
+//  Created by Luc Sciammetta on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct Lottery_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
